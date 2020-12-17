9186ce8ab14caf55ada4bce2d6002704ea71f86e
kvzivn <hello@kevinivan.com>
2020-12-17 01:10:32 +0100

    use svg for modal close btn

--- src/components/shopbtn.js
+++ src/components/shopbtn.js
@@ -87,29 +87,18 @@ export default (props) => {
 				<button
 					onClick={() => setModalOpen(false)}
 					sx={{
-						display: 'flex',
-						alignItems: 'center',
-						justifyContent: 'center',
 						position: 'absolute',
 						top: '12px',
 						right: '12px',
-						width: '32px',
-						height: '32px',
-						px: 0,
-						pb: '6px',
-						fontSize: '42px',
-						backgroundColor: 'white',
-						color: 'text',
-						borderColor: '#44474F',
-						borderRadius: '50%',
+						background: 'none',
+						border: 'none',
 						cursor: 'pointer',
 						':focus': {
-							outline: 'none',
-							border: '2px solid #ccc'
+							outline: 'none'
 						}
 					}}
 				>
-					✕
+					<svg width="34" height="34"><g transform="translate(1 1)" fill="none" fill-rule="evenodd"><circle stroke="#44474F" fill="#FFF" cx="16" cy="16" r="16"/><path d="M22.679 10.94L17.619 16l5.06 5.06-1.62 1.619L16 17.618l-5.06 5.06-1.619-1.619 5.06-5.06-5.06-5.058 1.62-1.62L16 14.381l5.06-5.06 1.619 1.62z" fill="#44474F"/></g></svg>
 				</button>
 
 				<Styled.h1 sx={{
