854119004e0901ce4a5bf806267d439b5784cb33
Kevin <hello@kevinivan.com>
2021-03-18 21:38:58 +0100

    fix horizontal scroll bug

--- src/sections/textsliders.js
+++ src/sections/textsliders.js
@@ -1,13 +1,13 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import TextSliderTop from '../components/textslidertop'
-import TextSliderMid from '../components/textslidermid'
-import TextSliderBottom from '../components/textsliderbottom'
+import { jsx } from "theme-ui"
+import TextSliderTop from "../components/textslidertop"
+import TextSliderMid from "../components/textslidermid"
+import TextSliderBottom from "../components/textsliderbottom"
 
 export default () => (
-	<section sx={{ mt: [6, 6, 11] }}>
-		<TextSliderTop />
-		<TextSliderMid />
-		<TextSliderBottom />
-	</section>
+  <section sx={{ maxWidth: "100%", overflow: "hidden", mt: [6, 6, 11] }}>
+    <TextSliderTop />
+    <TextSliderMid />
+    <TextSliderBottom />
+  </section>
 )
