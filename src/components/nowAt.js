0a92b77e90aecad38fd2736f3edfb78aefd1079c
Kevin <hello@kevinivan.com>
2021-06-15 12:01:01 +0200

    make banner loop infinitely

--- src/components/nowAt.js
+++ src/components/nowAt.js
@@ -1,17 +1,28 @@
 /** @jsx jsx */
 import { jsx, Styled, Container, Flex } from "theme-ui"
-import { useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
 import { keyframes } from '@emotion/react'
 
 const slide = keyframes({
   from: { transform: 'translateX(0)' },
-  to: { transform: 'translateX(-92%)' }
+  to: { transform: 'translateX(calc(-110px * 11))' }
 })
 
 export default () => {
   const {
-    stores
+    stores,
+    costco,
+    atlantic,
+    dominion,
+    extra,
+    independent,
+    loblaws,
+    maxi,
+    provigo,
+    superstore,
+    valu,
+    zehrs
    } = useStaticQuery(
     graphql`
       query {
@@ -22,12 +33,90 @@ export default () => {
             }
           }
         }
+        costco: file(relativePath: { eq: "stores/costco.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
+        atlantic: file(relativePath: { eq: "stores/atlantic.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
+        dominion: file(relativePath: { eq: "stores/dominion.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
+        extra: file(relativePath: { eq: "stores/extra.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
+        independent: file(relativePath: { eq: "stores/independent.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
+        loblaws: file(relativePath: { eq: "stores/loblaws.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
+        maxi: file(relativePath: { eq: "stores/maxi.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
+        provigo: file(relativePath: { eq: "stores/provigo.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
+        superstore: file(relativePath: { eq: "stores/superstore.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
+        valu: file(relativePath: { eq: "stores/valu.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
+        zehrs: file(relativePath: { eq: "stores/zehrs.png" }) {
+          childImageSharp {
+            fixed(width: 110, quality: 100) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
       }
     `
   )
 
   return (
-    <div sx={{
+    <Link to="/stores" sx={{
+      display: 'block',
       background: '#241f21'
     }}>
       <Container sx={{
@@ -57,47 +146,127 @@ export default () => {
           <Flex sx={{
             position: 'relative',
             alignItems: 'center',
-            width: '3201px',
+            width: 'calc(110px * 22)',
             height: '100%',
             animationName: slide,
-            animationTimingFunction: 'ease-in-out',
+            animationTimingFunction: 'linear',
             animationDuration: '30s',
             animationDelay: '2s',
             animationIterationCount: 'infinite',
-            animationDirection: 'alternate',
           }}>
             <Img
-              fixed={stores.childImageSharp.fixed}
-              imgStyle={{
-                objectFit: 'fill'
-              }}
-              sx={{
-                width: '100%'
-              }}
+              fixed={costco.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
             />
             <Img
-              fixed={stores.childImageSharp.fixed}
-              imgStyle={{
-                objectFit: 'fill'
-              }}
-              sx={{
-                width: '100%',
-                ml: 4
-              }}
+              fixed={loblaws.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
             />
             <Img
-              fixed={stores.childImageSharp.fixed}
-              imgStyle={{
-                objectFit: 'fill'
-              }}
-              sx={{
-                width: '100%',
-                ml: 4
-              }}
+              fixed={superstore.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={provigo.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={dominion.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={extra.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={maxi.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={independent.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={valu.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={zehrs.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={atlantic.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={costco.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={loblaws.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={superstore.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={provigo.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={dominion.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={extra.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={maxi.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={independent.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={valu.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={zehrs.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
+            />
+            <Img
+              fixed={atlantic.childImageSharp.fixed}
+              imgStyle={{ objectFit: 'fill' }}
+              sx={{ width: '100%' }}
             />
           </Flex>
         </div>
       </Container>
-    </div>
+    </Link>
   )
 }
