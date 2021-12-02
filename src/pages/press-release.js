c5400546644507f2770bb58b6749410d549253a3
Ambursley <eloise.bucais@gmail.com>
2021-12-02 17:35:38 -0500

    Updated copy

--- src/pages/press-release.js
+++ src/pages/press-release.js
@@ -35,10 +35,10 @@ export default () => {
       </Container>
       <Container sx={{ width: ["100%", "100%", "950px"], mt: [4, 4, 8] }}>
         <Styled.h2 sx={{ lineHeight: [1.15, 1.15, "59px !important"] }}>
-          Oat Canada Announces new Partnership with Canada soccer
+          Oat Canada Announces New Partnership with Canada Soccer
         </Styled.h2>
         <Styled.h3 sx={{ mt: 2 }}>
-          Official plant protein partner of Canada soccer
+          Official Plant Protein Partner of Canada Soccer and Canadian Premier League
         </Styled.h3>
         <Flex
           sx={{
@@ -110,38 +110,15 @@ export default () => {
         />
         <section sx={{ mt: [6, 6, "60px"] }}>
           <Text sx={{ mt: [4, 4, 6] }}>
-            Toronto, ON (November 19, 2021) – Canada Soccer and Oat Canada
-            announced today an exclusive multi-year partnership encompassing
-            Canada Soccer’s National Teams and the Canadian Premier League
-            (CPL). Oat Canada will be the “Official Plant Protein Partner” of
-            Canada Soccer ahead of what is scheduled to be a very busy 2022 year
-            including FIFA World Cup Qatar 2022™ Qualifiers matches and FIFA
-            Women’s World Cup 2023™ Qualifers. Through this new partnership,
-            this rapidly growing food and beverage company will become an
-            integrated partner with both Canada Soccer and the Canadian Premier
-            League and its eight clubs.
+            Toronto, ON (December 7, 2021) – Oat Canada and Canada Soccer have announced an exclusive multi-year partnership encompassing Canada Soccer’s National Teams and the Canadian Premier League (CPL).  Oat Canada will be the “Official Plant Protein Partner” of Canada Soccer ahead of what is scheduled to be a very busy 2022 year including FIFA World Cup Qatar 2022™ Qualifiers matches and FIFA Women’s World Cup 2023™ Qualifiers. This partnership supports Oat Canada's strategic direction to deliver higher protein plant-based products with maximum nutritional value for athletes and consumers alike across Canada and beyond. 
           </Text>
 
           <Text sx={{ mt: [4, 4, 6] }}>
-            The agreement, negotiated by Canadian Soccer Business (CSB) will
-            feature Oat Canada as the presenting sponsor of one Canada Soccer’s
-            National Team matches per year, including a FIFA World Cup Qatar
-            2022™ Qualifiers match. Oat Canada will also become an official
-            partner of the 2022 editions of the Canadian Championship and Toyota
-            National Championships.
+            This news release comes ahead of Oat Canada’s disruptive innovation project announcement – a project which is expected to benefit both consumers and companies in the food and beverage industry. In addition to the Canada Soccer partnership, Oat Canada is preparing to launch a suite of new products in 2022.  
           </Text>
 
           <Text sx={{ mt: [4, 4, 6] }}>
-            <i>
-              “Canada Soccer is excited to welcome such a dynamic and innovative
-              company like Oat Canada to our family of partners,”
-            </i>{" "}
-            said Peter Montopoli, Canada Soccer General Secretary.{" "}
-            <i>
-              “The interest and momentum for the sport in our country has never
-              been higher and we look forward to growing together with Oat
-              Canada.”
-            </i>
+            Oat Canada will be featured as a presenting sponsor of one Canada Soccer National Team match per year, including a FIFA World Cup Qatar 2022™ Qualifiers match. Oat Canada will also be an official partner of the 2022 editions of the Canadian Championship and Toyota National Championships.   
           </Text>
 
           <Text sx={{ mt: [4, 4, 6] }}>
@@ -165,11 +142,24 @@ export default () => {
           </Text>
 
           <Text sx={{ mt: [4, 4, 6] }}>
-            Oat Canada will have coast to coast reach through a full suite of
-            sponsorships assets with all eight Canadian Premier League clubs
-            including match day brand integration across each CPL match and
-            original content and editorial on CanPL channels, providing fans
-            with more engaging and informative updates.
+            <i>
+              “Canada Soccer is excited to welcome such a dynamic and innovative
+              company like Oat Canada to our family of partners,”
+            </i>{" "}
+            said Peter Montopoli, Canada Soccer General Secretary.{" "}
+            <i>
+              “The interest and momentum for the sport in our country has never
+              been higher and we look forward to growing together with Oat
+              Canada.”
+            </i>
+          </Text>
+
+          <Text sx={{ mt: [4, 4, 6] }}>
+            Oat Canada is the first plant protein company to partner with Canada Soccer, a timely collaboration as the world increasingly shifts towards plant-based choices. With this new plant protein partnership, Oat Canada is setting a new standard for functional foods and leading the way to a plant-powered and more sustainable future.
+          </Text>
+
+          <Text sx={{ mt: [4, 4, 6] }}>
+            Oat Canada will also have coast to coast reach through a full suite of sponsorships assets with all eight Canadian Premier League clubs including match day brand integration across each CPL match and original content and editorial on CanPL channels, providing fans with more engaging and informative updates.
           </Text>
 
           <Text sx={{ mt: [4, 4, 6] }}>
@@ -188,6 +178,26 @@ export default () => {
           </Text>
         </section>
         <section sx={{ mt: [6, 6, "60px"] }}>
+         
+          <Box sx={{ mt: 6 }}>
+            <Text sx={{ mb: "4px", fontWeight: "bold" }}>About Oat Canada</Text>
+            <Text>
+              Oat Canada is a rapidly growing food and beverage Startup from
+              Toronto, Ontario. The company introduced the first Zero Sugar oat
+              milk in 2020. Only a year later, with one product they are now
+              available in over +1,300 stores across Canada, including major
+              retailers such as Loblaws, Costco East, Longo’s and Whole Foods
+              (January 2022). Oat Canada was also featured on CBC’s Dragons’
+              Den, where they received investment offers from all six Dragons.
+              Oat Canada is committed to becoming the leader in oat
+              protein-based products, producing low sugar, protein enriched food
+              and beverages that challenge soccer players to sustain their
+              health and the planet. For more information about Oat Canada,
+              visit <a href="https://oatcanada.com">oatcanada.com.</a>
+            </Text>
+          </Box>
+          
+          
           <Box>
             <Text sx={{ mb: "4px", fontWeight: "bold" }}>
               About Canada Soccer
@@ -215,24 +225,6 @@ export default () => {
               by Canadians that brings professional soccer to communities
               coast-to-coast. For updates and more information, please go to{" "}
               <a href="https://canpl.ca">CanPL.ca.</a>
-            </Text>
-          </Box>
-
-          <Box sx={{ mt: 6 }}>
-            <Text sx={{ mb: "4px", fontWeight: "bold" }}>About Oat Canada</Text>
-            <Text>
-              Oat Canada is a rapidly growing food and beverage Startup from
-              Toronto, Ontario. The company introduced the first Zero Sugar oat
-              milk in 2020. Only a year later, with one product they are now
-              available in over +1,300 stores across Canada, including major
-              retailers such as Loblaws, Costco East, Longo’s and Whole Foods
-              (January 2022). Oat Canada was also featured on CBC’s Dragons’
-              Den, where they received investment offers from all six Dragons.
-              Oat Canada is committed to becoming the leader in oat
-              protein-based products, producing low sugar, protein enriched food
-              and beverages that challenge soccer players to sustain their
-              health and the planet. For more information about Oat Canada,
-              visit <a href="https://oatcanada.com">oatcanada.com.</a>
             </Text>
           </Box>
 
