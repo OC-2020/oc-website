6b7111369207ee1479fc0056702dd19d698e2265
Ambursley <eloise.bucais@gmail.com>
2021-12-08 20:20:21 -0500

    hopefully this deploys now...

--- src/pages/about.js
+++ src/pages/about.js
@@ -44,27 +44,72 @@ export default () => {
 
         
           <Text sx={{ mt: [4, 4, 6] }}>
-            Oat Canada was fundamentally built on small good habits. We believe that when you make one good habit, it has a rippling effect, influencing other areas of your life. When my husband and I got married, we had started a simple habit – save 25% or more of our monthly income. By the end of the year, we ended up saving more than 50% of our annual income. Because of one simple change, our entire lifestyle changed as well. We started cooking more meals at home, exercising daily, and consiously checking nutrition facts tables. As we optimized our lives in small ways, we became more aware of how we cared for our health as well as how we treated the planet. This lead to the choice to consioucly reducing our sugar consumption where ever possible and include more plant proteins into our diets—and that’s how we got introduced to oat milk!
+            Oat Canada was fundamentally built on small good habits. We believe 
+            that when you make one good habit, it has a rippling effect, 
+            influencing other areas of your life. When my husband and I got 
+            married, we had started a simple habit—save 25% or more of our 
+            monthly income. By the end of the year, we ended up saving more than 
+            50% of our annual income. Because of one simple change, our entire 
+            lifestyle changed as well. We started cooking more meals at home, 
+            exercising daily, and consiously checking nutrition facts tables. As 
+            we optimized our lives in small ways, we became more aware of how we 
+            cared for our health as well as how we treated the planet. This lead 
+            to the choice to consioucly reducing our sugar consumption where ever 
+            possible and include more plant proteins into our diets—and that’s 
+            how we got introduced to oat milk!
           </Text>
 
           <Text sx={{ mt: [4, 4, 6] }}>
-            When we discovered oat milk it worked great for Jamari (because he’s lactose intolerant) and myself (I have a nut allergry), except for the fact that most oat milks were loaded with sugar. This was especially important to us because we come from families who suffer from diabetes—Jamari’s grandma would especially be on his case about watching his sugar intake because he’s legit a sugar addict. So, we tried to find ways to cut out sugar as much as possible, especially in things where it just didn’t make sense to have a lot in—like milk. 
+            When we discovered oat milk it worked great for Jamari (because he’s 
+            lactose intolerant) and myself (I have a nut allergry), except for 
+            the fact that most oat milks were loaded with sugar. This was especially 
+            important to us because we come from families who suffer from diabetes
+            —Jamari’s grandma would especially be on his case about watching his 
+            sugar intake because he’s legit a sugar addict. So, we tried to find ways 
+            to cut out sugar as much as possible, especially in things where it just 
+            didn’t make sense to have a lot in—like milk. 
           </Text>
 
           <Text sx={{ mt: [4, 4, 6] }}>
-            We looked and looked for a zero sugar oat milk option but got tired of misleading labels like <i>“unsweetened”</i>{" "} or <i>“no sugar added”</i>{" "} on cartons with sugar contents as high as 10 grams to 17 grams of sugar in one serving. To give some perspective, that’s nearly half of the sugar found in a can of coke. So, we decided to take matters into our own hands. Thus, Oat Canada was born.
+            We looked and looked for a zero sugar oat milk option but got tired of 
+            misleading labels like 
+            <i>
+            “unsweetened”
+            </i>{" "} 
+            or {" "}
+            <i>
+            “no sugar added”
+            </i>{" "}
+            on cartons with sugar contents as high as 10 grams to 17 grams of sugar 
+            in one serving. To give some perspective, that’s nearly half of the sugar 
+            found in a can of coke. So, we decided to take matters into our own hands. 
+            Thus, Oat Canada was born.
           </Text>
 
           <Text sx={{ mt: [4, 4, 6] }}>
-            Our promise is to be transparent with how much sugar is in our products, so that’s why you see a giant zero on the front of our packaging—no guessing and no confusion from misleading labels. We’re proud to have launched the first zero sugar and keto-friendly barista oat milk in the market—especially to have launched our business right in the middle of the pandemic in 2020.
+            Our promise is to be transparent with how much sugar is in our products, 
+            so that’s why you see a giant zero on the front of our packaging—no 
+            guessing and no confusion from misleading labels. We’re proud to have 
+            launched the first zero sugar and keto-friendly barista oat milk in the 
+            market—especially to have launched our business right in the middle of the 
+            pandemic in 2020.
           </Text>
 
           <Text sx={{ mt: [4, 4, 6] }}>
-            Today, just a year later after launch, we are now available in over 1,300 stores across Canada, including major retailers such as Loblaws, Costco East, Longo’s and Whole Foods (January 2022). An incredible feet considering Oat Canada only has one product in market at moment!
+            Today, just a year later after launch, we are now available in over 1,300 
+            stores across Canada, including major retailers such as Loblaws, Costco East, 
+            Longo’s and Whole Foods (January 2022). An incredible feet considering Oat 
+            Canada only has one product in market at moment!
           </Text>
 
           <Text sx={{ mt: [4, 4, 6] }}>
-            We’ve had the privilege to be featured on {" "}<a href="https://www.cbc.ca/dragonsden/m_pitches/oat-canada">CBC’s Dragons’ Den</a>. (Didn’t see the episode? Watch to find out what happened!) We’ve also been featured on {" "}<a href="https://vegnews.com/2021/8/sugar-free-oat-milk">VegNews</a>, {" "}<a href="https://issuu.com/ensembleiq/docs/cg07_2021?fr=sNjRhZjgwODQ2MQ">Canadian Grocer</a>, {" "}<a href="https://www.blogto.com/eat_drink/2020/06/toronto-zero-sugar-oat-milk-costco/">BlogTO</a>, {" "}<a href="https://www.thestar.com/local-vaughan/life/food-wine/2021/02/25/neighbourhood-eats-vaughan-markham-business-programs-help-couple-launch-oat-milk.html">Toronto Star</a>, and more. More recently, we’ve announced our exclusive partnership with Canada Soccer and the Canadian Premiere League! Oat Canada will be their “Official Plant Protein Partner”! Read all about it {" "}<a href="https://oatcanada.com/oat-canada-announces-new-partnership-with-canada-soccer/">here</a>.
+            We’ve had the privilege to be featured on <a href="https://www.cbc.ca/dragonsden/m_pitches/oat-canada">CBC’s Dragons’ Den</a>. 
+            (Didn’t see the episode? Watch to find out what happened!) We’ve also been featured on 
+            <a href="https://vegnews.com/2021/8/sugar-free-oat-milk">VegNews</a>, 
+            <a href="https://issuu.com/ensembleiq/docs/cg07_2021?fr=sNjRhZjgwODQ2MQ">Canadian Grocer</a>, 
+            <a href="https://www.blogto.com/eat_drink/2020/06/toronto-zero-sugar-oat-milk-costco/">BlogTO</a>, 
+            <a href="https://www.thestar.com/local-vaughan/life/food-wine/2021/02/25/neighbourhood-eats-vaughan-markham-business-programs-help-couple-launch-oat-milk.html">Toronto Star</a>, 
+            and more. More recently, we’ve announced our exclusive partnership with Canada Soccer and the Canadian Premiere League! Oat Canada will be their “Official Plant Protein Partner”! Read all about it <a href="https://oatcanada.com/oat-canada-announces-new-partnership-with-canada-soccer/">here</a>.
           </Text>
           </section>
           
