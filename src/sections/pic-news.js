76521438893d9d53fc8bb3f49848395a13a41645
Ambursley <eloise.bucais@gmail.com>
2022-03-28 18:58:39 -0400

    1

--- src/sections/pic-news.js
+++ src/sections/pic-news.js
@@ -119,7 +119,7 @@ export default () => {
             <Styled.h2
               sx={{
                 color: "white",
-                fontSize: ["27px", "27px", "81px"],
+                fontSize: ["27px", "27px", "1px"],
                 lineHeight: [null, null, "86px"],
               }}
             >
