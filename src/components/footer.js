525599404a7fe45e69cbacb2477231ce6c8ef7c9
Ambursley <eloise.bucais@gmail.com>
2023-01-04 13:50:06 -0500

    new year

--- src/components/footer.js
+++ src/components/footer.js
@@ -236,9 +236,11 @@ export default () => {
             },
           }}
         >
-          <p>
-          Copyright © <script>document.write(new Date().getFullYear());</script>Oat Canada.<br /> All Rights Reserved.
-          </p>   
+          
+          Copyright © <script type="text/javascript">
+  document.write(new Date().getFullYear());
+</script>Oat Canada.<br /> All Rights Reserved.
+            
           <a
                 href="https://www.termsfeed.com/live/379ea87a-1737-4ce7-b990-6abddb5d9a87"
                 target="_blank"
